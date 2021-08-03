664d9bbf9ce0db601ca3bfca91dfdd6378a39653
Sebastian Valencia <sebastianvalencia.ceron@gmail.com>
2021-08-03 18:36:46 -0500

    Conexion a DB y primera prueba

---
+++ src/main/java/web/ServletControlador.java
@@ -0,0 +1,26 @@
+package web;
+
+import datos.ClienteDaoJDBC;
+import dominio.Cliente;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/ServletControlador")
+public class ServletControlador extends HttpServlet{
+    
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        List<Cliente> clientes = new ClienteDaoJDBC().listar();
+//        Cliente cliente = new Cliente(1, "Sebastian", "Valencia", "qwer", "asdf", 23.00);
+        
+//        clientes.add(cliente);
+        System.out.println("clientes = " + clientes);
+        request.setAttribute("clientes", clientes);
+        request.getRequestDispatcher("clientes.jsp").forward(request, response);
+        
+    }
+}
